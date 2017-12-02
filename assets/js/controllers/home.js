61a5cb7f41cc8c845adff21625697c876546c7e7
Luciano José <luciano.jsjr@gmail.com>
2017-12-01 23:49:12 -0300

    Remove Map functions from Home

--- assets/js/controllers/home.js
+++ assets/js/controllers/home.js
@@ -5,21 +5,8 @@ angular
 HomeController.$inject = ['$scope', 'Utils'];
 
 function HomeController($scope, Utils) {
-	let currentIndex;
-	const mapElement = document.getElementById('map');
-	const markerIcon = {
-		path: google.maps.SymbolPath.CIRCLE,
-		scale: 4,
-		strokeColor: '#000000',
-		fillColor: 'orange',
-		fillOpacity: 1,
-	};
-
-	//TODO: RENDERIZAR A REDE ATUAL
-	//TODO: CRIAR VARIAVEL PARA REDE ATUAL
-
-	$scope.map;
 	$scope.currentIndex;
+	$scope.currentNetwork;
 	$scope.files = [];
 	$scope.openedFiles = [];
 	$scope.options = {
@@ -29,21 +16,11 @@ function HomeController($scope, Utils) {
 	};
 
 	init = () => {
-		$scope.map = new google.maps.Map(mapElement, {
-			center: {
-				lat: -8.05428,
-				lng: -34.8813
-			},
-			zoom: 6,
-			disableDefaultUI: true
-		});
-
 		$('.button-collapse').sideNav({
 			closeOnClick: true
 		});
 	};
 
-	//TODO: ATUALIZAR A REDE ATUAL
 	openFile = (index) => {
 		if (isOpened(index)) {
 			return;
@@ -55,19 +32,23 @@ function HomeController($scope, Utils) {
 		});
 
 		$scope.currentIndex = $scope.openedFiles.length - 1;
+		$scope.currentNetwork = $scope.files[index];
 
 		$('.button-collapse').sideNav('hide');
 	};
 
-	//TODO: ATUALIZAR INDEX AO REMOVER ARQUIVO
-	//TODO: ATUALIZAR A REDE ATUAL
 	closeFile = (index) => {
 		$scope.openedFiles.splice(index, 1);
+
+		$scope.currentIndex = $scope.openedFiles.length - 1;
+		$scope.currentNetwork = $scope.files[$scope.currentIndex];
 	};
 
-	//TODO: ATUALIZAR A REDE ATUAL
 	selectFile = (index) => {
+		const fileIndex = $scope.openedFiles[index].file_index;
+
 		$scope.currentIndex = index;
+		$scope.currentNetwork = $scope.files[fileIndex];
 	};
 
 	loadFile = (content) => {
@@ -77,108 +58,6 @@ function HomeController($scope, Utils) {
 		});
 
 		$('#file').val('');
-	};
-
-	renderNetwork = (index) => {
-		if (currentIndex === index) {
-			return;
-		}
-
-		currentIndex = index;
-		$scope.currentFile = $scope.files[currentIndex];
-
-		renderNodes();
-		renderEdges();
-	};
-
-	renderNodes = () => {
-		let marker;
-
-		$scope.currentFile.network.nodes.forEach((node, index) => {
-			$scope.map.setCenter({
-				lat: node.Latitude,
-				lng: node.Longitude
-			});
-
-			marker = new google.maps.Marker({
-				position: {
-					lat: node.Latitude,
-					lng: node.Longitude
-				},
-				icon: markerIcon,
-				draggable: true,
-				map: $scope.map
-			});
-
-			bindDrag(marker, index);
-		});
-	};
-
-	renderEdges = () => {
-		let edge;
-
-		$scope.currentFile.network.edges.forEach((edge, index) => {
-			let source;
-			let target;
-
-			source = findNode(edge.source, $scope.currentFile.network.nodes);
-			target = findNode(edge.target, $scope.currentFile.network.nodes);
-
-			edge = new google.maps.Polyline({
-				path: [
-					new google.maps.LatLng(source.Latitude, source.Longitude),
-					new google.maps.LatLng(target.Latitude, target.Longitude)
-				],
-				strokeColor: "#000000",
-				strokeOpacity: 1.0,
-				strokeWeight: 2,
-				map: $scope.map
-			});
-
-			source.edges_source.push(edge);
-			target.edges_target.push(edge);
-		});
-	};
-
-	bindDrag = (marker, index) => {
-		google.maps.event.addListener(marker, 'drag', (evt) => {
-			const newLatLng = {
-				lat: evt.latLng.lat(),
-				lng: evt.latLng.lng()
-			};
-
-			$scope.currentFile.network.nodes[index].Latitude = newLatLng.lat;
-			$scope.currentFile.network.nodes[index].Longitude = newLatLng.lng;
-
-			updateEdgesPosition(index, newLatLng);
-		});
-	};
-
-	updateEdgesPosition = (index, position) => {
-		if ($scope.currentFile.network.nodes[index].edges_source) {
-			$scope.currentFile.network.nodes[index].edges_source.forEach((edge) => {
-				let targetPoint = edge.getPath().getArray()[1];
-
-				edge.setPath([position, targetPoint]);
-			});
-		}
-
-		if ($scope.currentFile.network.nodes[index].edges_target) {
-			$scope.currentFile.network.nodes[index].edges_target.forEach((edge) => {
-				let sourcePoint = edge.getPath().getArray()[0];
-
-				edge.setPath([sourcePoint, position]);
-			});
-		}
-	};
-
-	findNode = (id, nodes) => {
-		return nodes.find((node) => {
-			node.edges_source = node.edges_source ? node.edges_source : [];
-			node.edges_target = node.edges_target ? node.edges_target : [];
-
-			return node.id === id;
-		});
 	};
 
 	isOpened = (index) => {
@@ -191,7 +70,6 @@ function HomeController($scope, Utils) {
 		$scope.options[feature] = !$scope.options[feature];
 	};
 
-	$scope.showMenu = true;
 	$scope.properties = {
 		"Número de nós": 1,
 		"Número de enlaces": 1,
@@ -220,5 +98,4 @@ function HomeController($scope, Utils) {
 	$scope.selectFile = selectFile;
 	$scope.loadFile = loadFile;
 	$scope.turnFeature = turnFeature;
-	$scope.renderNetwork = renderNetwork;
 }
