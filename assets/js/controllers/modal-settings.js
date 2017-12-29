f310bb39fe9e7dda7be1e751554c8bdc752b60a9
Luciano José <luciano.jsjr@gmail.com>
2017-12-29 14:00:00 -0300

    Add Validation on Settings modal

--- assets/js/controllers/modal-settings.js
+++ assets/js/controllers/modal-settings.js
@@ -12,6 +12,10 @@ function SettingsController($scope, $uibModalInstance) {
 	$scope.settings = $scope.settings ? JSON.parse($scope.settings) : [];
 
 	add = () => {
+		if(!isValid()){
+			return;
+		}
+
 		$scope.settings.push({
 			name: $scope.name,
 			url: $scope.url,
@@ -25,6 +29,10 @@ function SettingsController($scope, $uibModalInstance) {
 	};
 
 	save = () => {
+		if(!isValid()){
+			return;
+		}
+
 		$scope.settings[editIndex] = {
 			name: $scope.name,
 			url: $scope.url,
@@ -51,6 +59,20 @@ function SettingsController($scope, $uibModalInstance) {
 		$scope.url = $scope.settings[editIndex].url;
 		$scope.parametersUrl = $scope.settings[editIndex].parameters_url;
 		localStorage.settings = JSON.stringify($scope.settings);
+	};
+
+	isValid = () => {
+		if (!$scope.name) {
+			Materialize.toast('É preciso informar o nome da configuração.', 4000, 'toast-danger');
+			return false;
+		}
+
+		if (!$scope.url) {
+			Materialize.toast('É preciso informar a url de simulação.', 4000, 'toast-danger');
+			return false;
+		}
+
+		return true;
 	};
 
 	cancel = () => {
